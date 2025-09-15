3a4a9513c3a5e7f66b957473e5b82b7cb6729f7f
Mark Conroy <mark@mark.ie>
2025-09-15 12:11:21 +0100

    initial commit

---
+++ js/localgov-colour-picker-fields.js
@@ -0,0 +1,41 @@
+(function localgovColourPickerFieldsScript(Drupal) {
+  Drupal.behaviors.colourPickerFields = {
+    attach: function (context, settings) {
+      context = context || document;
+
+      const colourPickerFields = context.querySelectorAll('.colour-picker-field');
+
+      colourPickerFields.forEach(item => {
+        const text = item.querySelector('.colour-picker-field__text');
+        const picker = item.querySelector('.colour-picker-field__picker');
+
+        function handleSetColours(item1 = text, item2 = picker) {
+          let colour = item1.value;
+          item2.value = colour;
+          text.style.border = `5px solid ${colour}`;
+          item.classList.add('colour-picker-field--active');
+          picker.style.height = `60px`;
+          if (text.value) {
+            item.classList.add('colour-picker-field--active');
+          } else {
+            item.classList.remove('colour-picker-field--active');
+          }
+        }
+
+        if (text.value) {
+          handleSetColours();
+        }
+        picker.addEventListener('input', () => {
+          handleSetColours(picker, text);
+        });
+        picker.addEventListener('click', () => {
+          handleSetColours(picker, text);
+        });
+        text.addEventListener('change', () => {
+          handleSetColours();
+        });
+
+      })
+    },
+  };
+})(Drupal);
